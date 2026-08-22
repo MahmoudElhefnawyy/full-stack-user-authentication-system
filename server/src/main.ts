90bb466f989cb54101cd6e7f96a3dd4cce2dfaa9
Mahmoud Salem <mahmoudmohamedhef@gmail.com>
2026-08-22 23:22:20 +0300

    feat: add global HttpExceptionFilter for consistent error response shape

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,11 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT', 3000);
+
+  // Normalises all HTTP errors to a consistent { statusCode, message, path, timestamp } shape
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   // Activates class-validator decorators globally and strips unknown request fields
   app.useGlobalPipes(
